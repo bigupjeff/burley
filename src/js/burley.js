abf438a1fa111e4199d3fc9d3d68745124e9e680
wordpressjeff <me@jeffersonreal.com>
2023-12-14 13:14:26 +0000

    image slider css

--- src/js/burley.js
+++ src/js/burley.js
@@ -5,9 +5,7 @@
 import { slideHeader } from './frontend/_slide-header'
 import { mobilePopupMenu } from './frontend/_mobile-popup-menu'
 import { revealAnimation } from './frontend/_reveal-animation'
-import { logoSlider } from './frontend/_logo-slider'
 
 slideHeader()
 mobilePopupMenu()
 revealAnimation()
-logoSlider()
