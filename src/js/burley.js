677e1f6a6297eb937488da5c8ae27d776d4ec20f
wordpressjeff <me@jeffersonreal.com>
2023-12-14 07:42:10 +0000

    bug fixes

--- src/js/burley.js
+++ src/js/burley.js
@@ -5,7 +5,9 @@
 import { slideHeader } from './frontend/_slide-header'
 import { mobilePopupMenu } from './frontend/_mobile-popup-menu'
 import { revealAnimation } from './frontend/_reveal-animation'
+import { logoSlider } from './frontend/_logo-slider'
 
 slideHeader()
 mobilePopupMenu()
 revealAnimation()
+logoSlider()
